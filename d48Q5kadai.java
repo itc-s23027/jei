2bca43fb4028af2d1c2b73ee41fd90264cc318d1
itc-s23027 <s23027@std.it-college.ac.jp>
2024-05-31 12:33:20 +0900

    kadai

---
+++ d48Q5kadai.java
@@ -0,0 +1,15 @@
+public class d45Q5 {
+		public static void main(String[] args){
+				int[]numbers = new int[args.length];
+				
+				for (int i = 0;  ; i++) {
+						number[i] = Integer.parseInt(args[i]);
+				}
+
+				for (   ; nextMinIndex++) {
+						int minIndex = nextMinIndex;
+						int minValue = numbers[nextMinIndex];
+						for (int checkIndex = nextMinIndex + 1;
+										checkIndex < numbers.length; checkIndex++) {
+								if (
+
