4ee35ecdf302f7cda9b3e42ecac0b53ea9df41ac
itc-s23027 <s23027@std.it-college.ac.jp>
2024-06-14 03:44:59 +0000

    kadai

---
+++ d49Q26kadai.java
@@ -0,0 +1,11 @@
+public class d49Q26kadai {
+	public static void main(String[] args) {
+		d49Q26kadai obj = new d49Q26kadai();
+		obj.show(5.5);
+	}
+	
+	void show(double n) {
+		System.out.println("value is " + n);
+	}
+}
+
